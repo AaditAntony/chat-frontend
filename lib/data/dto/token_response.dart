6973400e372b578b3ccbabb7da55f8de3e78947b
Aadit Antony <aaditantony1112@gmail.com>
2025-12-04 19:42:18 +0530

    the tokenresponse is added and the login response is updated

---
+++ lib/data/dto/token_response.dart
@@ -0,0 +1,13 @@
+class TokenResponse {
+  final String token;
+
+  TokenResponse({
+    required this.token,
+  });
+
+  factory TokenResponse.fromJson(Map<String, dynamic> json) {
+    return TokenResponse(
+      token: json['token'] as String,
+    );
+  }
+}
